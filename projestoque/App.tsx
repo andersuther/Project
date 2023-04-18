7bc52147a77fe7649348322462add87a89ba0c1c
Anderson Fernandes <108153983+andersuther@users.noreply.github.com>
2023-04-18 11:56:50 -0300

    first commit

---
+++ projestoque/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ImageBackground, Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const projestoque: React.FC = () => {
+  return <View style={{flex:1}}>
+    <Text>Projeto Estoque</Text>
+  </View>;
+}
+
+export default projestoque;
